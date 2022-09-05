178d007b64219a4cbec6e5ef547cea2047bc444c
ferdiansyah <ferdikings03@gmail.com>
2022-09-05 22:37:39 +0800

    first commit

---
+++ login_js.js
@@ -0,0 +1,147 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC7lRYm2owotHi6ZcF9pf51CjDhPcH_zvg",
+    authDomain: "ifwebtoons-project.firebaseapp.com",
+    databaseURL: "https://ifwebtoons-project.firebaseio.com",
+    projectId: "ifwebtoons-project",
+    storageBucket: "ifwebtoons-project.appspot.com",
+    messagingSenderId: "158158880289",
+    appId: "1:158158880289:web:50c47debdbc5efea61ad60",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  const auth = firebase.auth()
+  const database = firebase.database()
+  
+  // Set up our register function
+  function register () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    full_name = document.getElementById('full_name').value
+    favourite_comic = document.getElementById('favourite_comic').value
+    from_indonesia = document.getElementById('from_indonesia').value
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+      // Don't continue running the code
+    }
+    if (validate_field(full_name) == false || validate_field(favourite_comic) == false || validate_field(from_indonesia) == false) {
+      alert('One or More Extra Fields is Outta Line!!')
+      return
+     
+    }
+   
+    // Move on with Auth
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        email : email,
+        full_name : full_name,
+        favourite_comic : favourite_comic,
+        from_indonesia : from_indonesia,
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('usersifweb/' + user.uid).set(user_data)
+  
+      // DOne
+      alert('User Created!!')
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  
+  // Set up our login function
+  function login () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+      // Don't continue running the code
+    }
+  
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('usersifweb/' + user.uid).update(user_data)
+  
+      // DOne
+      window.location.href = "http://www.w3schools.com";
+  
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  
+  
+  
+  
+  // Validate Functions
+  function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+      // Email is good
+      return true
+    } else {
+      // Email is not good
+      return false
+    }
+  }
+  
+  function validate_password(password) {
+    // Firebase only accepts lengths greater than 6
+    if (password < 6) {
+      return false
+    } else {
+      return true
+    }
+  }
+  
+  function validate_field(field) {
+    if (field == null) {
+      return false
+    }
+  
+    if (field.length <= 0) {
+      return false
+    } else {
+      return true
+    }
+  }
